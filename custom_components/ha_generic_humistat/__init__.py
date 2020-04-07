1d69eb688380ce9923c04d27ed3f32f453c7eaab
Igor Tkachenko <IATkachenko@gmail.com>
2020-04-07 20:23:05 +0300

    Added module files based on generic termostat

---
+++ custom_components/ha_generic_humistat/__init__.py
@@ -0,0 +1,1 @@
+"""The generic_humistat component."""
---
+++ custom_components/ha_generic_humistat/climate.py
@@ -0,0 +1,467 @@
+"""Adds support for generic humistat units."""
+import asyncio
+import logging
+
+import voluptuous as vol
+
+from homeassistant.components.climate import PLATFORM_SCHEMA, ClimateDevice
+from homeassistant.components.climate.const import (
+    ATTR_PRESET_MODE,
+    CURRENT_HVAC_DRY,
+    CURRENT_HVAC_HUMIDIFY,
+    CURRENT_HVAC_IDLE,
+    CURRENT_HVAC_OFF,
+    HVAC_MODE_DRY,
+    HVAC_MODE_HUMIDIFY,
+    HVAC_MODE_OFF,
+    PRESET_AWAY,
+    PRESET_NONE,
+    SUPPORT_PRESET_MODE,
+    SUPPORT_TARGET_HUMIDITY,
+)
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    ATTR_TEMPERATURE,
+    CONF_NAME,
+    EVENT_HOMEASSISTANT_START,
+    PRECISION_HALVES,
+    PRECISION_TENTHS,
+    PRECISION_WHOLE,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+    STATE_ON,
+    STATE_UNKNOWN,
+)
+from homeassistant.core import DOMAIN as HA_DOMAIN, callback
+from homeassistant.helpers import condition
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.event import (
+    async_track_state_change,
+    async_track_time_interval,
+)
+from homeassistant.helpers.restore_state import RestoreEntity
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_TOLERANCE = 5
+DEFAULT_NAME = "Generic Humistat"
+
+CONF_HUMIDIFYER = "humidifyer"
+CONF_SENSOR = "target_sensor"
+CONF_MIN_HUMIDITY = "min_humidity"
+CONF_MAX_HUMIDITY = "max_humidity"
+CONF_TARGET_HUMIDITY = "target_humidity"
+CONF_AC_MODE = "ac_mode"
+CONF_MIN_DUR = "min_cycle_duration"
+CONF_DRYNESS_TOLERANCE = "dryness_tolerance"
+CONF_OVERMOIST_TOLERANCE = "overmoist_tolerance"
+CONF_KEEP_ALIVE = "keep_alive"
+CONF_INITIAL_HVAC_MODE = "initial_hvac_mode"
+CONF_AWAY_HUMIDITY = "away_humidity"
+CONF_PRECISION = "precision"
+SUPPORT_FLAGS = SUPPORT_TARGET_HUMIDITY
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_HUMIDIFYER): cv.entity_id,
+        vol.Required(CONF_SENSOR): cv.entity_id,
+        vol.Optional(CONF_AC_MODE): cv.boolean,
+        vol.Optional(CONF_MAX_HUMIDITY): vol.Coerce(float),
+        vol.Optional(CONF_MIN_DUR): vol.All(cv.time_period, cv.positive_timedelta),
+        vol.Optional(CONF_MIN_HUMIDITY): vol.Coerce(float),
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_DRYNESS_TOLERANCE, default=DEFAULT_TOLERANCE): vol.Coerce(float),
+        vol.Optional(CONF_OVERMOIST_TOLERANCE, default=DEFAULT_TOLERANCE): vol.Coerce(float),
+        vol.Optional(CONF_TARGET_HUMIDITY, default=40): vol.Coerce(float),
+        vol.Optional(CONF_KEEP_ALIVE): vol.All(cv.time_period, cv.positive_timedelta),
+        vol.Optional(CONF_INITIAL_HVAC_MODE): vol.In(
+            [HVAC_MODE_DRY, HVAC_MODE_HUMIDIFY, HVAC_MODE_OFF]
+        ),
+        vol.Optional(CONF_AWAY_HUMIDITY): vol.Coerce(float),
+        vol.Optional(CONF_PRECISION): vol.In(
+            [PRECISION_TENTHS, PRECISION_HALVES, PRECISION_WHOLE]
+        ),
+    }
+)
+
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the generic thermostat platform."""
+    name = config.get(CONF_NAME)
+    humidifyer_entity_id = config.get(CONF_HUMIDIFYER)
+    sensor_entity_id = config.get(CONF_SENSOR)
+    min_humidity = config.get(CONF_MIN_HUMIDITY)
+    max_humidity = config.get(CONF_MAX_HUMIDITY)
+    target_humidity = config.get(CONF_TARGET_HUMIDITY)
+    ac_mode = config.get(CONF_AC_MODE)
+    min_cycle_duration = config.get(CONF_MIN_DUR)
+    dryness_tolerance = config.get(CONF_DRYNESS_TOLERANCE)
+    overmoist_tolerance = config.get(CONF_OVERMOIST_TOLERANCE)
+    keep_alive = config.get(CONF_KEEP_ALIVE)
+    initial_hvac_mode = config.get(CONF_INITIAL_HVAC_MODE)
+    away_humidity = config.get(CONF_AWAY_HUMIDITY)
+    precision = config.get(CONF_PRECISION)
+    unit = '%'
+
+    async_add_entities(
+        [
+            GenericHumistat(
+                name,
+                humidifyer_entity_id,
+                sensor_entity_id,
+                min_humidity,
+                max_humidity,
+                target_humidity,
+                ac_mode,
+                min_cycle_duration,
+                dryness_tolerance,
+                overmoist_tolerance,
+                keep_alive,
+                initial_hvac_mode,
+                away_humidity,
+                precision,
+                unit,
+            )
+        ]
+    )
+
+
+class GenericHumistat(ClimateDevice, RestoreEntity):
+    """Representation of a Generic Humistat device."""
+
+    def __init__(
+        self,
+        name,
+        humidifyer_entity_id,
+        sensor_entity_id,
+        min_humidity,
+        max_humidity,
+        target_humidity,
+        ac_mode,
+        min_cycle_duration,
+        dryness_tolerance,
+        overmoist_tolerance,
+        keep_alive,
+        initial_hvac_mode,
+        away_humidity,
+        precision,
+        unit,
+    ):
+        """Initialize the humistat."""
+        self._name = name
+        self.humidifyer_entity_id = humidifyer_entity_id
+        self.sensor_entity_id = sensor_entity_id
+        self.ac_mode = ac_mode
+        self.min_cycle_duration = min_cycle_duration
+        self._dryness_tolerance = dryness_tolerance
+        self._overmoist_tolerance = overmoist_tolerance
+        self._keep_alive = keep_alive
+        self._hvac_mode = initial_hvac_mode
+        self._saved_target_humidity = target_humidity or away_humidity
+        self._humidity_precision = precision
+        if self.ac_mode:
+            self._hvac_list = [HVAC_MODE_DRY, HVAC_MODE_OFF]
+        else:
+            self._hvac_list = [HVAC_MODE_HUMIDIFY, HVAC_MODE_OFF]
+        self._active = False
+        self._cur_humidity = None
+        self._humidity_lock = asyncio.Lock()
+        self._min_humidity = min_humidity
+        self._max_humidity = max_humidity
+        self._target_humidity = target_humidity
+        self._unit = unit
+        self._support_flags = SUPPORT_FLAGS
+        if away_humidity:
+            self._support_flags = SUPPORT_FLAGS | SUPPORT_PRESET_MODE
+        self._away_humidity = away_humidity
+        self._is_away = False
+
+    async def async_added_to_hass(self):
+        """Run when entity about to be added."""
+        await super().async_added_to_hass()
+
+        # Add listener
+        async_track_state_change(
+            self.hass, self.sensor_entity_id, self._async_sensor_changed
+        )
+        async_track_state_change(
+            self.hass, self.humidifyer_entity_id, self._async_switch_changed
+        )
+
+        if self._keep_alive:
+            async_track_time_interval(
+                self.hass, self._async_control_moisturizing, self._keep_alive
+            )
+
+        @callback
+        def _async_startup(event):
+            """Init on startup."""
+            sensor_state = self.hass.states.get(self.sensor_entity_id)
+            if sensor_state and sensor_state.state != STATE_UNKNOWN:
+                self._async_update_sumidity(sensor_state)
+
+        self.hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, _async_startup)
+
+        # Check If we have an old state
+        old_state = await self.async_get_last_state()
+        if old_state is not None:
+            # If we have no initial humidity, restore
+            if self._target_humidity is None:
+                # If we have a previously saved humidity 
+                if old_state.attributes.get(ATTR_HUMIDITY) is None:
+                    if self.ac_mode:
+                        self._target_humidity = self.max_humidity
+                    else:
+                        self._target_humidity = self.min_humidity
+                    _LOGGER.warning(
+                        "Undefined target humidity, falling back to %s",
+                        self._target_humidity,
+                    )
+                else:
+                    self._target_humidity = float(old_state.attributes[ATTR_HUMIDITY])
+            if old_state.attributes.get(ATTR_PRESET_MODE) == PRESET_AWAY:
+                self._is_away = True
+            if not self._hvac_mode and old_state.state:
+                self._hvac_mode = old_state.state
+
+        else:
+            # No previous state, try and restore defaults
+            if self._target_humidity is None:
+                if self.ac_mode:
+                    self._target_humidity = self.max_humidity
+                else:
+                    self._target_humidity = self.min_humidity
+            _LOGGER.warning(
+                "No previously saved humidity, setting to %s", self._target_humidity
+            )
+
+        # Set default state to off
+        if not self._hvac_mode:
+            self._hvac_mode = HVAC_MODE_OFF
+
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return False
+
+    @property
+    def name(self):
+        """Return the name of the thermostat."""
+        return self._name
+
+    @property
+    def precision(self):
+        """Return the precision of the system."""
+        if self._humidity_precision is not None:
+            return self._humidity_precision
+        return super().precision
+
+    @property
+    def humidity_unit(self):
+        """Return the unit of measurement."""
+        return self._unit
+
+    @property
+    def current_humidity(self):
+        """Return the sensor humidity."""
+        return self._cur_humidity
+
+    @property
+    def hvac_mode(self):
+        """Return current operation."""
+        return self._hvac_mode
+
+    @property
+    def hvac_action(self):
+        """Return the current running hvac operation if supported.
+
+        Need to be one of CURRENT_HVAC_*.
+        """
+        if self._hvac_mode == HVAC_MODE_OFF:
+            return CURRENT_HVAC_OFF
+        if not self._is_device_active:
+            return CURRENT_HVAC_IDLE
+        if self.ac_mode:
+            return CURRENT_HVAC_DRY
+        return CURRENT_HVAC_HUMIDIFY
+
+    @property
+    def target_humidity(self):
+        """Return the humidity we try to reach."""
+        return self._target_humidity
+
+    @property
+    def hvac_modes(self):
+        """List of available operation modes."""
+        return self._hvac_list
+
+    @property
+    def preset_mode(self):
+        """Return the current preset mode, e.g., home, away, temp."""
+        return PRESET_AWAY if self._is_away else PRESET_NONE
+
+    @property
+    def preset_modes(self):
+        """Return a list of available preset modes or PRESET_NONE if _away_humidity is undefined."""
+        return [PRESET_NONE, PRESET_AWAY] if self._away_humidity else PRESET_NONE
+
+    async def async_set_hvac_mode(self, hvac_mode):
+        """Set hvac mode."""
+        if hvac_mode == HVAC_MODE_HUMIDIFY:
+            self._hvac_mode = HVAC_MODE_HUMIDIFY
+            await self._async_control_moisturizing(force=True)
+        elif hvac_mode == HVAC_MODE_DRY:
+            self._hvac_mode = HVAC_MODE_DRY
+            await self._async_control_moisturizing(force=True)
+        elif hvac_mode == HVAC_MODE_OFF:
+            self._hvac_mode = HVAC_MODE_OFF
+            if self._is_device_active:
+                await self._async_humidifyer_turn_off()
+        else:
+            _LOGGER.error("Unrecognized hvac mode: %s", hvac_mode)
+            return
+        # Ensure we update the current operation after changing the mode
+        self.schedule_update_ha_state()
+
+    async def async_set_humidity(self, **kwargs):
+        """Set new target humidity."""
+        humidity = kwargs.get(ATTR_HUMIDITY)
+        if humidity is None:
+            return
+        self._target_humidity = humidity
+        await self._async_control_moisturizing(force=True)
+        await self.async_update_ha_state()
+
+    @property
+    def min_humidity(self):
+        """Return the minimum humidity."""
+        if self._min_humidity is not None:
+            return self._min_humidity
+
+        # get default humidity from super class
+        return super().min_humidity
+
+    @property
+    def max_humidity(self):
+        """Return the maximum humidity."""
+        if self._max_humidity is not None:
+            return self._max_humidity
+
+        # Get default humidity from super class
+        return super().max_humidity
+
+    async def _async_sensor_changed(self, entity_id, old_state, new_state):
+        """Handle humidity  changes."""
+        if new_state is None:
+            return
+
+        self._async_update_humidity(new_state)
+        await self._async_control_moisturizing()
+        await self.async_update_ha_state()
+
+    @callback
+    def _async_switch_changed(self, entity_id, old_state, new_state):
+        """Handle humidifyer switch state changes."""
+        if new_state is None:
+            return
+        self.async_schedule_update_ha_state()
+
+    @callback
+    def _async_update_humidity(self, state):
+        """Update thermostat with latest state from sensor."""
+        try:
+            self._cur_humidity = float(state.state)
+        except ValueError as ex:
+            _LOGGER.error("Unable to update from sensor: %s", ex)
+
+    async def _async_control_moisturizing(self, time=None, force=False):
+        """Check if we need to turn humidifying on or off."""
+        async with self._humidity_lock:
+            if not self._active and None not in (self._cur_humidity, self._target_humidity):
+                self._active = True
+                _LOGGER.info(
+                    "Obtained current and target humidity. "
+                    "Generic humistat active. %s, %s",
+                    self._cur_humidity,
+                    self._target_humidity,
+                )
+
+            if not self._active or self._hvac_mode == HVAC_MODE_OFF:
+                return
+
+            if not force and time is None:
+                # If the `force` argument is True, we
+                # ignore `min_cycle_duration`.
+                # If the `time` argument is not none, we were invoked for
+                # keep-alive purposes, and `min_cycle_duration` is irrelevant.
+                if self.min_cycle_duration:
+                    if self._is_device_active:
+                        current_state = STATE_ON
+                    else:
+                        current_state = HVAC_MODE_OFF
+                    long_enough = condition.state(
+                        self.hass,
+                        self.humidifyer_entity_id,
+                        current_state,
+                        self.min_cycle_duration,
+                    )
+                    if not long_enough:
+                        return
+
+            too_dry = self._target_humidity >= self._cur_humidity + self._dryness_tolerance
+            too_wet = self._cur_humidity >= self._target_humidity + self._overmoist_tolerance
+            if self._is_device_active:
+                if (self.ac_mode and too_dry) or (not self.ac_mode and too_wet):
+                    _LOGGER.info("Turning off humidifyer %s", self.humidifyer_entity_id)
+                    await self._async_humidifyer_turn_off()
+                elif time is not None:
+                    # The time argument is passed only in keep-alive case
+                    _LOGGER.info(
+                        "Keep-alive - Turning on humidifyer %s",
+                        self.humidifyer_entity_id,
+                    )
+                    await self._async_humidifyer_turn_on()
+            else:
+                if (self.ac_mode and too_wet) or (not self.ac_mode and too_dry):
+                    _LOGGER.info("Turning on humidifyer %s", self.humidifyer_entity_id)
+                    await self._async_humidifyer_turn_on()
+                elif time is not None:
+                    # The time argument is passed only in keep-alive case
+                    _LOGGER.info(
+                        "Keep-alive - Turning off humidifyer %s", self.humidifyer_entity_id
+                    )
+                    await self._async_humidifyer_turn_off()
+
+    @property
+    def _is_device_active(self):
+        """If the toggleable device is currently active."""
+        return self.hass.states.is_state(self.humidifyer_entity_id, STATE_ON)
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return self._support_flags
+
+    async def _async_humidifyer_turn_on(self):
+        """Turn heater toggleable device on."""
+        data = {ATTR_ENTITY_ID: self.humidifyer_entity_id}
+        await self.hass.services.async_call(HA_DOMAIN, SERVICE_TURN_ON, data)
+
+    async def _async_humidifyer_turn_off(self):
+        """Turn heater toggleable device off."""
+        data = {ATTR_ENTITY_ID: self.humidifyer_entity_id}
+        await self.hass.services.async_call(HA_DOMAIN, SERVICE_TURN_OFF, data)
+
+    async def async_set_preset_mode(self, preset_mode: str):
+        """Set new preset mode."""
+        if preset_mode == PRESET_AWAY and not self._is_away:
+            self._is_away = True
+            self._saved_target_humidity = self._target_humidity
+            self._target_humidity = self._away_humidity
+            await self._async_control_moisturizing(force=True)
+        elif preset_mode == PRESET_NONE and self._is_away:
+            self._is_away = False
+            self._target_humidity = self._saved_target_humidity
+            await self._async_control_moisturizing(force=True)
+
+        await self.async_update_ha_state()
